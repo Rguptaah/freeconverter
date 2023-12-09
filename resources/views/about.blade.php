670c60515ad76db6ed93095c7c5b8598d2b2b502
sidarya321@gmail.com <sidarya321@gmail.com>
2023-12-09 12:40:06 +0530

    views update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('content')
+    <div class="container">
+        <p>Hii about page</p>
+        <h1>test</h1>
+    </div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('content')
+    <div class="container">
+        <p>Hii welcome</p>
+        <h1>test</h1>
+    </div>
+@endsection
